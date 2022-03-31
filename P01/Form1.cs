5185d5786b5a49267d2eb5eced5c0151d188522b
Jan Polách <polachj.05@spst.cz>
2022-03-31 10:51:56 +0200

    P01 + P03 done P04 start

--- P01/Form1.cs
+++ P01/Form1.cs
@@ -16,5 +16,33 @@ namespace P01
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string samohlasky = "aeiouyáéěíóúůý"; 
+            string souhlasky = "bcčdďfghjklmnpqrřsštťvwxzž";
+            int pocet_sa = 0;
+            int pocet_so = 0;
+            int nepismeno = 0;
+            string text = textBox1.Text;
+            foreach(char pismeno in text)
+            {
+                if (samohlasky.Contains(pismeno))
+                {
+                    pocet_sa++;
+                }
+                else
+                {
+                    if (souhlasky.Contains(pismeno))
+                    {
+                        pocet_so++;
+                    }
+                }
+            }
+            nepismeno = text.Length - (pocet_sa + pocet_so);
+            label4.Text = pocet_sa.ToString();
+            label5.Text = pocet_so.ToString();
+            label6.Text = nepismeno.ToString();
+        }
     }
 }
--- P03/Form1.cs
+++ P03/Form1.cs
@@ -16,5 +16,39 @@ namespace P03
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int k=0;
+            bool kex = false;
+            string text = textBox1.Text;
+            foreach (char p in textBox1.Text)
+            {
+                if (p >= '0' && p <= '9')
+                {
+                    k = Convert.ToInt32(p-48);
+                    kex = true;
+                    break;
+                }
+            
+            }
+            label1.Text = k.ToString();
+            if (kex)
+            {
+                
+                
+                    if (k < text.Length)
+                    {
+                        text = text.Remove(text.Length - k);
+                    }
+                    else
+                        text = "";
+                
+            }
+            if (kex)
+                textBox2.Text = text;
+            else
+                textBox2.Text = "V zadaném stringu neexistuje číslo";
+        }
     }
 }

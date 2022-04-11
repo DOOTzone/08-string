a6f2bfdc5addbf8b934bc1c4f6b8f254116af3d1
Jan Polách <polachj.05@spst.cz>
2022-04-11 09:10:34 +0200

    p09

--- P09/Form1.Designer.cs
+++ P09/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -51,15 +52,16 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(231, 192);
             this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "to je debil\r\nno t";
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
             this.checkBox1.Location = new System.Drawing.Point(12, 212);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.Size = new System.Drawing.Size(97, 17);
             this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.Text = "Velká písmena";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // checkBox2
@@ -67,16 +69,26 @@
             this.checkBox2.AutoSize = true;
             this.checkBox2.Location = new System.Drawing.Point(12, 235);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.Size = new System.Drawing.Size(52, 17);
             this.checkBox2.TabIndex = 3;
-            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.Text = "tečky";
             this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.textBox1);
@@ -94,6 +106,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- P09/Form1.cs
+++ P09/Form1.cs
@@ -19,7 +19,29 @@ namespace P09
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string[] vety = textBox1.Lines
+            string[] vety = textBox1.Lines.ToArray();
+            string novaVeta="";
+            for(int i =0;i<textBox1.Lines.Length;i++)
+            {
+                if (checkBox1.Checked)
+                {
+                    char pismeno = vety[i][0];
+                    string veta_s = vety[i].Substring(1,vety[i].Length-1);
+                    pismeno = char.ToUpper(pismeno);
+                    label1.Text = pismeno.ToString();
+                    novaVeta += pismeno+veta_s;
+                }
+                    if (checkBox2.Checked)
+                {
+                    char dot = (novaVeta[novaVeta.Length - 1]);
+                    if (dot != '.')
+                        novaVeta += ".";
+                }
+                    if(i+1!=textBox1.Lines.Length)
+                novaVeta+=Environment.NewLine;
+                MessageBox.Show(novaVeta, "nova veta");
+            }
+            textBox1.Text = novaVeta;
         }
     }
 }
